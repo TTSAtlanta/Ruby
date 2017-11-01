2c36002ca6c5a927133eef65da733fffd4bbe55f
Zapata <rzapata@gmail.com>
2017-11-01 18:40:57 -0400

    Added list courses

--- student_registration.rb
+++ student_registration.rb
@@ -2,30 +2,18 @@
 @courses = Array.new
 
 class Student
+  attr_accessor :name, :age 
   def initialize(name, age)
   	@name = name
   	@age = age
   end
-
-  def name
-  	@name
-  end
-  def age
-  	@age
-  end
 end
 
 class Course
+  attr_accessor :name, :description 
   def initialize(name, description)
   	@name = name
   	@description = description
-  end
-
-  def name
-  	@name
-  end
-  def description
-  	@description
   end
 end
 
@@ -33,10 +21,16 @@ def print_menu
 	puts "Enter a selection from the menu: "
 	puts "1. Add a Student"
 	puts "2. List students"
+  puts "3. Create a course"
+  puts "4. List courses"
+  puts "5. Assign Student to Course"
+  puts "6. Print student roster"
+  puts "E: Exit "
+
 	@choice = gets.chomp.upcase
 end 
 
-def imnotagoodpersontopick
+def add_a_student
   puts "Enter the student's name: "
   name = gets.chomp
   puts "Enter the student's age: "
@@ -47,10 +41,9 @@ def imnotagoodpersontopick
   @students.push(student)
 
   puts "******* Student added *******" #adds a student
-
 end
 
-def yomama #lists students
+def list_students #lists students
   puts "The students in the system are: "
   @students.each_with_index do |student, index|
     puts "#{index+1}. Name: #{student.name} :: Age: #{student.age}" 
@@ -70,17 +63,27 @@ def add_a_course
   puts "******* Course added *******" #adds a course	
 end
 
+def list_courses #lists students
+  puts "The courses in the system are: "
+  @courses.each_with_index do |course, index|
+    puts "#{index+1}. Name: #{course.name} :: Description: #{course.description}" 
+  end
+end
+
 while @choice != "E" do 
 	
 	if @choice == "1"
-	  imnotagoodpersontopick
-      print_menu
+	  add_a_student
+    print_menu
 	elsif @choice == "2"
-	  yomama
+	  list_students
 	  print_menu
 	elsif @choice == "3"
-      add_a_course
-      print_menu		
+    add_a_course
+    print_menu	
+  elsif @choice == "4"
+    list_courses
+    print_menu    	
 	else
 	  puts "Please select from the menu"
 	  print_menu	
