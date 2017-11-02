bfdd6b583528fba0c5369a7dc93f03b1564fb8cf
Zapata <rzapata@gmail.com>
2017-11-01 20:13:01 -0400

    Added student registration

--- student_registration.rb
+++ student_registration.rb
@@ -1,25 +1,20 @@
-@students = []
+require_relative 'student'
+require_relative 'course'
+
+
+@students = Array.new
 @courses = Array.new
 @student_courses = Hash.new
 
-class Student
-  attr_accessor :name, :age 
-  def initialize(name, age)
-  	@name = name
-  	@age = age
-  end
-end
 
-class Course
-  attr_accessor :name, :description 
-  def initialize(name, description)
-  	@name = name
-  	@description = description
-  end
-end
+def print_menu(option)
+	if option == "Y"
+    system 'clear'
+  end 
 
-def print_menu
-	puts "Enter a selection from the menu: "
+  puts "*********************************"
+  puts "Enter a selection from the menu: "
+  puts "*********************************"
 	puts "1. Add a Student"
 	puts "2. List students"
   puts "3. Create a course"
@@ -82,35 +77,42 @@ def assign_student_to_course
 
   @student_courses[@students[student_choice-1]] = @courses[course_choice-1]
   
+  puts "Student assigned!"
+
+end 
+
+def print_student_roster
+  puts "This is the student roster and their classes: "
+
   @student_courses.each do |key, value|
     puts "#{key.name} is assigned to #{value.name}"
   end
-
-
-
-
-end 
+  
+end
 
 while @choice != "E" do 
 	
 	if @choice == "1"
 	  add_a_student
-    print_menu
+    print_menu("Y")
 	elsif @choice == "2"
 	  list_students
-	  print_menu
+	  print_menu("N")
 	elsif @choice == "3"
     add_a_course
-    print_menu	
+    print_menu("Y")	
   elsif @choice == "4"
     list_courses
-    print_menu 
+    print_menu("N") 
   elsif @choice == "5"
     assign_student_to_course
-    print_menu      	
+    print_menu("Y")  
+  elsif @choice == "6"
+    print_student_roster
+    print_menu("N")      	
 	else
 	  puts "Please select from the menu"
-	  print_menu	
+	  print_menu("Y")	
 	end
 end
 	
